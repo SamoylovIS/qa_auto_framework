8375e11072d7c2bfe7f6acc17120abb79f774f3b
Samoylov <samoylov125rus@mail.ru>
2022-09-22 14:05:45 +0300

    update

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -1,13 +1,13 @@
-
+from selenium.webdriver.common.by import By
 
 class TextBoxPageLocators:
-
     #form fields
-    FULL_NAME = (By.CSS_SELECTOR, "input[id='userName-label']")
-    EMAIL = (By.CSS_SELECTOR, "input[id='userEmail-label']")
-    CURRENT_ADDRESS = (By.CSS_SELECTOR, "input[id='currentAddress-label']")
-    PERMANENT_ADRESS = (By.CSS_SELECTOR, "input[id='permanentAddress-label']")
-    SUBMIT = (By.CSS_SELECTOR, "input[id='submit']")
+
+    FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='permanentAddress']")
+    SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
 
     #created from
     CREATED_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,8 +1,14 @@
+import time
+
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
-class TextBoxPages(BasePage):
+class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys()
+        self.element_is_visible(self.locators.FULL_NAME).send_keys('fds')
+        self.element_is_visible(self.locators.EMAIL).send_keys('fds@mail.com')
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('fds')
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('fds')
+        self.element_is_visible(self.locators.SUBMIT).click()
